c71b1e1e577fa9a3b630edb95cf5e4095c9423a0
kaleem2408 <109723776+kaleem2408@users.noreply.github.com>
2024-01-24 22:49:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
 min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
 sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
 
-},1001);
+},1000);
 
